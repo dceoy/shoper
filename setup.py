fb7d436816adfed70677454003d335520222fa15
dceoy <dnarsil+github@gmail.com>
2019-12-17 03:59:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,28 +4,29 @@ from setuptools import find_packages, setup
 
 from shopy import __version__
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='shopy',
     version=__version__,
-    description='Simple shell operator module',
-    packages=find_packages(),
     author='Daichi Narushima',
     author_email='dnarsil+github@gmail.com',
+    description='Simple shell operator module',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/dceoy/shopy',
+    packages=find_packages(),
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Plugins',
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Shells'
     ],
-    long_description="""\
-shopy
------
-
-Simple shell operator module for Python
-"""
+    python_requires='>=3.5'
 )
